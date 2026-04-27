9f5383a8ebcf7e229176bed70c9576f6c95b7927
0-s0g0 <sogo.rei812@gmail.com>
2026-04-27 17:25:55 +0900

    ambrt2

--- app/components/NewsCard/NewsCard.tsx
+++ app/components/NewsCard/NewsCard.tsx
@@ -27,7 +27,7 @@ const getTagBgColor = (tag: string) => {
     case "Event":
       return "bg-purple-500"
     case "Adoption":
-      return "bg-amber-400"
+      return "bg-amber-500"
     default:
       return "bg-slate-600"
   }
